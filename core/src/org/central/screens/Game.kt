a3c7dcfcea6946c15a98d0aadee327e936c25584
jojo <jojomickymack@gmail.com>
2019-05-16 05:09:58 -0600

    again, glClear not needed - actor was replaced by texture

--- core/src/org/central/screens/Game.kt
+++ core/src/org/central/screens/Game.kt
@@ -55,8 +55,6 @@ class Game(val app: App) : KtxScreen {
 
         //Render the whole scene in the FBO
         fbo.begin()
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         app.sb.begin()
 
         // Draw lightshafts
