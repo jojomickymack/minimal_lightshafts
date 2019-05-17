e6d63b0cc1f893af02bd2e4202ce34eb1b0a20f9
jojo <jojomickymack@gmail.com>
2019-05-16 21:53:18 -0600

    definitely rendering faster on android, however projection matrix is messed up

--- core/src/org/central/screens/Game.kt
+++ core/src/org/central/screens/Game.kt
@@ -2,11 +2,14 @@ package org.central.screens
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import ktx.app.KtxScreen
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.Texture.TextureFilter
 import com.badlogic.gdx.graphics.glutils.FrameBuffer
 import com.badlogic.gdx.graphics.glutils.ShaderProgram
+import com.badlogic.gdx.math.Matrix4
 import org.central.App
 
 
@@ -14,24 +17,26 @@ class Game(val app: App) : KtxScreen {
 
     private var window = Texture("small_window_wall.png")
 
-    private val occludersFBO = FrameBuffer(Pixmap.Format.RGBA8888, app.width.toInt(), app.height.toInt(), false)
+    private val occludersFbo = FrameBuffer(Pixmap.Format.RGBA8888, app.width.toInt(), app.height.toInt(), false)
     private val occlusionApprox = FrameBuffer(Pixmap.Format.RGB888, app.width.toInt(), app.height.toInt(), false)
 
     private val occlusionApproxShader = ShaderProgram(Gdx.files.internal("shaders/default.vert"), Gdx.files.internal("shaders/occlusion.frag"))
 
+    private val postProcProj = Matrix4().setToOrtho2D(0f, 0f, app.width, app.height)
+
     override fun render(delta: Float) {
 
         // draws the black shape that blocks out the light
-        occludersFBO.begin()
+        occludersFbo.begin()
 
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
 
         app.sb.begin()
         app.sb.draw(window, Gdx.input.x.toFloat() - window.width / 2, app.height - Gdx.input.y.toFloat() - window.height / 2)
         app.sb.end()
 
-        occludersFBO.end()
+        occludersFbo.end()
 
         // calculates all of the lightrays
         occlusionApprox.begin()
@@ -39,7 +44,7 @@ class Game(val app: App) : KtxScreen {
 
         app.sb.begin()
         occlusionApproxShader.setUniformf("cent", 0.5f, 0.5f)
-        app.sb.draw(occludersFBO.colorBufferTexture, 0f, 0f, app.width, app.height, 0f, 0f, 1f, 1f)
+        app.sb.draw(occludersFbo.colorBufferTexture, 0f, 0f, app.width, app.height, 0f, 0f, 1f, 1f)
         app.sb.end()
 
         occlusionApprox.end()
@@ -50,13 +55,20 @@ class Game(val app: App) : KtxScreen {
 
         // Apply post processing
         app.sb.begin()
+
+        app.sb.projectionMatrix = postProcProj
+        app.sb.disableBlending()
+
         app.sb.draw(occlusionApprox.colorBufferTexture, 0f, 0f, app.width, app.height, 0f, 0f, 1f, 1f)
+
+        app.sb.projectionMatrix = app.cam.combined
+        app.sb.enableBlending()
         app.sb.end()
     }
 
     override fun dispose() {
         window.dispose()
-        occludersFBO.dispose()
+        occludersFbo.dispose()
         occlusionApprox.dispose()
         occlusionApproxShader.dispose()
     }
